79e7ad14bbcc902afce569910cb1f212561e3365
youssefheshamm <youssef_hesham@aucegypt.edu>
2025-07-17 03:48:50 +0300

    message leya

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"Message": " This is the root endpoint"}
+
+
+@app.get("/minus/{a}/{b}")
+def minus(a, b):
+    try:
+        a_num = float(a)
+        b_num = float(b)
+        result = a_num - b_num
+        if a_num.is_integer():
+            a_num = int(a_num)
+        if b_num.is_integer():
+            b_num = int(b_num)
+        if result.is_integer():
+            result = int(result)
+        return {f"a = {a}, b = {b}, a - b = {result}"}
+    except ValueError:
+        return {"Error": " Both values must be of numerical value"}
