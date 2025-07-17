65278c66fa4e83da75b1b935bca01aa9d64d008c
youssefheshamm <youssef_hesham@aucegypt.edu>
2025-07-17 04:43:09 +0300

    added palindrome

--- main.py
+++ main.py
@@ -77,3 +77,20 @@ def add_numbers(a, b):
         return {f"a = {a_num}, b = {b_num}, a × b = {result} "}
     except ValueError:
         return {"ERROR: both inputs must be numeric values"}
+
+
+@app.get("/pal/{x}")
+def palindrome(x):
+    y = x
+    try:
+        z = float(y)
+        return {"this is not a word"}
+    except ValueError:
+        if len(y) > 1:
+            y = y.lower()
+            for i in range(len(y)//2):
+                if y[i] != y[-(i+1)]:
+                    return {"The word is not a palindrome"}
+            return {"The word is a palindrome"}
+        else:
+            return {"The word is too short"}
