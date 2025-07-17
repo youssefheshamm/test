3559b03ac6d2abeb29f3d2c22bb144281c018a61
youssefheshamm <youssef_hesham@aucegypt.edu>
2025-07-17 03:59:11 +0300

    added new features

--- main.py
+++ main.py
@@ -26,3 +26,54 @@ def minus(a, b):
         return {f"a = {a}, b = {b}, a - b = {result}"}
     except ValueError:
         return {"Error": " Both values must be of numerical value"}
+
+
+@app.get("/add/{a}/{b}")
+def add_numbers(a, b):
+    try:
+        a_num = float(a)
+        b_num = float(b)
+        result = a_num + b_num
+        if a_num.is_integer():
+            a_num = int(a_num)
+        if b_num.is_integer():
+            b_num = int(b_num)
+        if result.is_integer():
+            result = int(result)
+        return {f"a = {a_num}, b = {b_num}, a + b = {result} "}
+    except ValueError:
+        return {"ERROR: both inputs must be numeric values"}
+
+
+@app.get("/div/{a}/{b}")
+def add_numbers(a, b):
+    try:
+        a_num = float(a)
+        b_num = float(b)
+        result = a_num / b_num
+        if a_num.is_integer():
+            a_num = int(a_num)
+        if b_num.is_integer():
+            b_num = int(b_num)
+        if result.is_integer():
+            result = int(result)
+        return {f"a = {a_num}, b = {b_num}, a / b = {result} "}
+    except ValueError:
+        return {"ERROR: both inputs must be numeric values"}
+
+
+@app.get("/mult/{a}/{b}")
+def add_numbers(a, b):
+    try:
+        a_num = float(a)
+        b_num = float(b)
+        result = a_num * b_num
+        if a_num.is_integer():
+            a_num = int(a_num)
+        if b_num.is_integer():
+            b_num = int(b_num)
+        if result.is_integer():
+            result = int(result)
+        return {f"a = {a_num}, b = {b_num}, a × b = {result} "}
+    except ValueError:
+        return {"ERROR: both inputs must be numeric values"}
